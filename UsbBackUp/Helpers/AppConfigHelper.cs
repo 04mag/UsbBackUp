bae42fc7fbb4fad529fd5512da8a9c541ed7becf
04mag <mathiasgreisen@gmail.com>
2024-11-02 11:11:39 +0100

    Made changes to app.config and added AppConfigHelper

---
+++ UsbBackUp/Helpers/AppConfigHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsbBackUp.Helpers
+{
+    public static class AppConfigHelper
+    {
+        private static string? GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
+        private static void SetSetting(string key, string value)
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings[key].Value = value;
+            configuration.Save(ConfigurationSaveMode.Full, true);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
